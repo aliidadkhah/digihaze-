fa0e4379cb88da5c2e9e4fabb368403ff56db619
aliidadkhah <8tagali999@gmail.com>
2026-08-24 15:43:54 +0330

    Update route.js

--- app/api/telegram/webhook/route.js
+++ app/api/telegram/webhook/route.js
@@ -1,5 +1,5 @@
 import { supabaseAdmin } from "@/lib/supabaseAdmin";
-
+export const dynamic = "force-dynamic";
 export async function POST(request) {
   try {
     const update = await request.json();
