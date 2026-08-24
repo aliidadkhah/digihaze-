fad9dc6e4c068b60d387975b3f5b675dc85b421b
aliidadkhah <8tagali999@gmail.com>
2026-08-24 22:31:41 +0330

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import HomeContent from "@/components/HomeContent";
 import { SITE_NAME, SITE_DESCRIPTION } from "@/lib/site";
 
 export const metadata = {
-  title: `${SITE_NAME} | فروشگاه مایع ویپ، دستگاه ویپ و پاد اورجینال`,
+  title: `${SITE_NAME} | فروشگاه پاد، سالت و کارتریج`,
   description: SITE_DESCRIPTION,
 };
 
