d3a5d11a96eff2b6790b2ef2e24aabe2a722abd9
aliidadkhah <8tagali999@gmail.com>
2026-08-29 14:05:30 +0330

    Create page.jsx

---
+++ app/terms/page.jsx
@@ -0,0 +1,11 @@
+import TermsContent from "@/components/TermsContent";
+
+export const metadata = {
+  title: "قوانین و شرایط استفاده",
+  description:
+    "قوانین و شرایط استفاده از سایت دیجی‌هیز شامل مقررات عمومی، حریم خصوصی، حساب کاربری و حقوق مالکیت محتوا.",
+};
+
+export default function TermsPage() {
+  return <TermsContent />;
+}
