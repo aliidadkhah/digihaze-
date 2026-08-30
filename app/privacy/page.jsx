ba53617064283c3c952260ff1844a149db5f80b8
aliidadkhah <8tagali999@gmail.com>
2026-08-30 12:12:51 +0330

    Create page.jsx

---
+++ app/privacy/page.jsx
@@ -0,0 +1,11 @@
+import PrivacyContent from "@/components/PrivacyContent";
+
+export const metadata = {
+  title: "حریم خصوصی",
+  description:
+    "سیاست حریم خصوصی دیجی‌هیز و نحوه‌ی جمع‌آوری، استفاده و حفاظت از اطلاعات کاربران.",
+};
+
+export default function PrivacyPage() {
+  return <PrivacyContent />;
+}
