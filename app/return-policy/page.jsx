460fefdc3281842c8e30cc712cc7ee72be809f47
aliidadkhah <8tagali999@gmail.com>
2026-08-30 12:11:33 +0330

    Create page.jsx

---
+++ app/return-policy/page.jsx
@@ -0,0 +1,11 @@
+import ReturnPolicyContent from "@/components/ReturnPolicyContent";
+
+export const metadata = {
+  title: "شرایط مرجوعی کالا",
+  description:
+    "شرایط و نحوه‌ی مرجوعی کالا در دیجی‌هیز، شامل مغایرت کالا، ایراد محصول، هزینه بازگشت و نحوه ثبت درخواست.",
+};
+
+export default function ReturnPolicyPage() {
+  return <ReturnPolicyContent />;
+}
