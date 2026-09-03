340b646de2ec083072f53a6e52dae1b0ca316d61
aliidadkhah <8tagali999@gmail.com>
2026-09-03 10:59:54 +0330

    Update CategoryBar.jsx

--- components/CategoryBar.jsx
+++ components/CategoryBar.jsx
@@ -362,8 +362,8 @@ export default function CategoryBar() {
 
         .category-bar-sub-item:hover {
           background: var(--surface2);
-          font-weight: 700;
-          font-size: 14.5px;
+          font-weight: 700 !important;
+          font-size: 14.5px !important;
         }
 
         .category-bar-item-row {
